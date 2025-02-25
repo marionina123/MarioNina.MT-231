703cf8d929465d5b5f7fbec3c8bd880e76256ccc
marionina123 <mario.nina@ucb.edu.bo>
2025-02-25 12:25:18 -0400

    Practica en clases 1

--- Practicas en clase/1. 25_02/practica1.sh
+++ Practicas en clase/1. 25_02/practica1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-   - Mostrar el directorio actual (`pwd`).  
-   - Acceder a la carpeta `/tmp` y listar su contenido (`ls`).  
-   - Volver al directorio personal (`cd ~`).  
-   - Crear una carpeta llamada `practica_shell` en el directorio personal (`mkdir`).  
-   - Acceder a `practica_shell` (`cd practica_shell`).  
-   - Mostrar el directorio actual (`pwd`). 
-.
+pwd
+cd /tmp
+ls
+cd ~
+mkdir  -p practica_shell
+cd patrica_shell
+pwd
